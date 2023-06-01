84c96000813dabd280d47be40527cd247d613911
Evgeny Koshelev <eko2000@gmail.com>
2023-06-01 17:59:33 +0200

    Initial Commit

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Evgeny Koshelev on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
