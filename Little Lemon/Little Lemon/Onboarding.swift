456069e9cdd66bfc1bc7a912c7ba733dfc370644
Evgeny Koshelev <eko2000@gmail.com>
2023-06-02 16:37:39 +0200

    Add user onboarding stuff

---
+++ Little Lemon/Little Lemon/Onboarding.swift
@@ -0,0 +1,64 @@
+//
+//  Onboarding.swift
+//  Little Lemon
+//
+//  Created by Evgeny Koshelev on 02.06.23.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var email = ""
+    @State var isLoggedIn = false
+
+    static let kFirstName = "first_name_key"
+    static let kLastName = "last_name_key"
+    static let kEmail = "email_key"
+    static let kIsLoggedIn = "is_logged_in"
+
+    func isValidEmail() -> Bool {
+        let emailRegex = try! Regex("^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$")
+        if let _ = try! emailRegex.wholeMatch(in: email) {
+            return true
+        }
+        return false
+    }
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                NavigationLink(isActive: $isLoggedIn, destination: { Home() }) {
+                    EmptyView()
+                }
+                TextField("First Name", text: $firstName)
+                    .autocorrectionDisabled(true)
+                TextField("Last Name", text: $lastName)
+                    .autocorrectionDisabled(true)
+                TextField("email", text: $email)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled(true)
+                Button("Register") {
+                    if !firstName.isEmpty && !lastName.isEmpty && isValidEmail() {
+                        UserDefaults.standard.set(firstName, forKey: Onboarding.kFirstName)
+                        UserDefaults.standard.set(lastName, forKey: Onboarding.kLastName)
+                        UserDefaults.standard.set(email, forKey: Onboarding.kEmail)
+                        UserDefaults.standard.set(isLoggedIn, forKey: Onboarding.kIsLoggedIn)
+                        isLoggedIn = true
+                    }
+                }
+            }
+            .padding()
+            .onAppear {
+                isLoggedIn = UserDefaults.standard.bool(forKey: Onboarding.kIsLoggedIn)
+            }
+        }
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
